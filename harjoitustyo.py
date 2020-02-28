6a24c2e9c3ff7cc1f21eb173bf7f086f1c1a64c3
JRamone <JMKoho@gmail.com>
2020-02-28 14:24:09 +0200

    Lisää tehokkuustesti

--- harjoitustyo.py
+++ harjoitustyo.py
@@ -1,7 +1,9 @@
 
 import sqlite3
 from datetime import datetime, time
+import time
 import re
+import random
 
 
 
@@ -15,10 +17,13 @@ c = kanta.cursor()
 
 def alusta(c):
     try:
-        c.execute("CREATE TABLE Paikat(id INTEGER PRIMARY KEY, nimi TEXT, UNIQUE(nimi))")
-        c.execute("CREATE TABLE Asiakkaat(id INTEGER PRIMARY KEY, nimi TEXT, UNIQUE(nimi))")
-        c.execute("CREATE TABLE Paketit(id INTEGER PRIMARY KEY,asiakas_id INTEGER, seurantakoodi TEXT, UNIQUE(seurantakoodi))")
-        c.execute("CREATE TABLE Tapahtumat(id INTEGER PRIMARY KEY,aika TEXT, paketti_id INTEGER, paikka_id INTEGER ,kuvaus TEXT)")
+        
+        c.execute("PRAGMA foreign_keys = ON;")
+        c.execute("CREATE TABLE Paikat(id INTEGER PRIMARY KEY, nimi TEXT, UNIQUE(nimi));")
+        c.execute("CREATE TABLE Asiakkaat(id INTEGER PRIMARY KEY, nimi TEXT, UNIQUE(nimi));")
+        c.execute("CREATE TABLE Paketit(id INTEGER PRIMARY KEY,asiakas_id INTEGER REFERENCES Asiakkaat, seurantakoodi TEXT, UNIQUE(seurantakoodi));")
+        c.execute("CREATE TABLE Tapahtumat(id INTEGER PRIMARY KEY,aika TEXT, paketti_id INTEGER REFERENCES Paketit, paikka_id INTEGER REFERENCES Paikat ,kuvaus TEXT);")
+        c.execute("CREATE INDEX idx_paketti_id ON Tapahtumat (paketti_id)")
         print("tietokanta luotu")
     except:
         print("Jotakin meni vikaan.")
@@ -26,16 +31,17 @@ def alusta(c):
 
 def lisaaPaikka(c):
     paikka = input("Anna paikan nimi:")
-    sql = "INSERT INTO Paikat (nimi) VALUES (?)"
     try:
-        c.execute("SELECT EXISTS(SELECT nimi FROM paikat WHERE nimi = ?)", (paikka,))
-        if (c.fetchone()[0] == 1):
+        c.execute("SELECT nimi FROM paikat WHERE nimi = ?", (paikka,))
+        onkopaikka = c.fetchone()
+        if (onkopaikka != None):
             print("VIRHE: Paikka on jo olemassa")
             return
-        c.execute(sql,(paikka,))
+        else:
+            c.execute("INSERT INTO Paikat (nimi) VALUES (?)", (paikka,))
         print("paikka lisätty")
     except:
-        print("Jotakin meni vikaan. Et voi lisätä kahta samannimistä paikkaa.")
+        print("Jotakin meni vikaan.")
     pass
 
 def lisaaAsiakas(c):
@@ -50,7 +56,7 @@ def lisaaAsiakas(c):
         c.execute(sql,args)
         print("Asiakas lisätty.")
     except:
-        print("Jotakin meni vikaan. Et voi lisätä kahta samannimistä asiakasta.")
+        print("Jotakin meni vikaan.")
     pass
 
 def lisaaPaketti(c):
@@ -112,14 +118,14 @@ def haeSeurantakoodilla(c):
     try:
         koodi = input("Anna seurantakoodi:")
         c.execute("SELECT id FROM Paketit WHERE seurantakoodi=?",(koodi,))
-        paketti_id = c.fetchone()[0]
+        paketti_id = c.fetchone()
         if paketti_id == None:
             print("Pakettia ei löytynyt")
         else:
-            c.execute("SELECT aika,nimi,kuvaus FROM Tapahtumat,Paikat WHERE Paikat.id = Tapahtumat.paikka_id AND paketti_id=?",(paketti_id,))
+            c.execute("SELECT aika,nimi,kuvaus FROM Tapahtumat,Paikat WHERE Paikat.id = Tapahtumat.paikka_id AND paketti_id=?",(paketti_id[0],))
             tiedot = c.fetchall()
             for x in tiedot:
-                print (x)
+                print (str(x[0]) + "\t" + str(x[1]) + "\t" + str(x[2]))
 
     except:
         print("Metodissa haeSeurantakoodilla() tapahtui odottamaton virhe.")
@@ -129,11 +135,11 @@ def haeAsiakkaanPaketit(c):
     try:
         asiakas = input("Anna asiakkaan nimi:")
         c.execute("SELECT id FROM Asiakkaat WHERE nimi=?",(asiakas,))
-        asiakas_id = c.fetchone()[0]
+        asiakas_id = c.fetchone()
         if asiakas_id == None:
             print("Asiakasta ei löytynyt")
         else:
-            c.execute("SELECT seurantakoodi, COUNT(paketti_id) FROM Paketit LEFT JOIN Tapahtumat ON Paketit.id = Tapahtumat.paketti_id WHERE asiakas_id = ? GROUP BY paketti_id",(asiakas_id,))
+            c.execute("SELECT seurantakoodi, COUNT(paketti_id) FROM Paketit LEFT JOIN Tapahtumat ON Paketit.id = Tapahtumat.paketti_id WHERE asiakas_id = ? GROUP BY paketti_id",(asiakas_id[0],))
             tiedot = c.fetchall()
             for x in tiedot:
                 print ("Seurantakoodi: " + str(x[0]) + "\t Tapahtumia yhteensä : " + str(x[1]) +"kpl")
@@ -144,7 +150,7 @@ def haeAsiakkaanPaketit(c):
 def haeTapahtumienMäärä(c):
     paikka = input("Anna paikan nimi:")
     c.execute("SELECT id FROM paikat WHERE nimi = ?", (paikka,))
-    paikka_id = c.fetchone()[0]
+    paikka_id = c.fetchone()
     if paikka_id == None:
         print("Paikkaa ei ole olemassa")
     else:
@@ -156,19 +162,60 @@ def haeTapahtumienMäärä(c):
         except:
             print("Syötit päivämäärän väärin.")
             return
-        c.execute("SELECT COUNT (id) FROM tapahtumat WHERE paikka_id=? AND aika LIKE ? GROUP BY paikka_id",(paikka_id,pvm3))
+        c.execute("SELECT COUNT (id) FROM tapahtumat WHERE paikka_id=? AND aika LIKE ? GROUP BY paikka_id",(paikka_id[0],pvm3))
         print("Tapahtumien määrä: " + str(c.fetchone()[0]))
 
     pass
 
 def suoritaTehokkuustesti(c):
+   
+    paikat = []
+    asiakkaat = []
+    paketit = []
+
+    alku_aika = time.time()
+
+    c.execute("BEGIN TRANSACTION")
+    for i in range(1000):
+        paikat.append("P" + str(i+1))
+        c.execute("INSERT INTO Paikat (nimi) VALUES (?)", (paikat[i],))
+    print("1000 Paikkaa lisätty ajassa: \t\t\t" + str(round(time.time() - alku_aika,6))+ " sek")
+
+    alku_aika = time.time()
+    for i in range(1000):
+        asiakkaat.append("A" + str(i+1))
+        c.execute("INSERT INTO Asiakkaat (nimi) VALUES (?)", (asiakkaat[i],))
+    print("1000 Asiakasta lisätty ajassa: \t\t\t" + str(round(time.time() - alku_aika,6))+ " sek")
+
+    alku_aika = time.time()
+    random.shuffle(asiakkaat)
+    for i in range(1000):
+        paketit.append("Paketti" + str(i+1))
+        c.execute("INSERT INTO Paketit (asiakas_id,seurantakoodi) VALUES (?,?)", (random.choice(asiakkaat)[1:],paketit[i])) #Asiakas_id ja seurantakoodi
+    print("1000 Pakettia lisätty ajassa: \t\t\t" + str(round(time.time() - alku_aika,6))+ " sek")
+
+    alku_aika = time.time()
+    tapahtumat = []
+    for i in range(1000000):
+        tapahtumat.append("Tapahtuma" + str(i+1))
+        c.execute("INSERT INTO Tapahtumat (aika,paketti_id,paikka_id,kuvaus) VALUES (?,?,?,?)", [datetime.now().strftime("%d-%m-%Y, %H:%M:%S"),random.choice(paketit)[7:],random.choice(paikat)[1:],tapahtumat[i]])
+    print("1000000 Tapahtumaa lisätty ajassa: \t\t" + str(round(time.time() - alku_aika,6))+ " sek")
+    c.execute("COMMIT")
+
+    alku_aika = time.time()
+    for i in range(1000):
+        c.execute("SELECT COUNT (id) FROM Paketit WHERE asiakas_id = ?", (random.choice(asiakkaat)[1:],))
+    print("1000 Pakettikyselyä suoritettu ajassa: \t\t" + str(round(time.time() - alku_aika,6))+ " sek")
+
+    alku_aika = time.time()
+    for i in range(1000):
+        c.execute("SELECT COUNT (paketti_id) FROM Tapahtumat WHERE paketti_id = ?", (random.choice(paketit)[7:],))
+    print("1000 Tapahtumakyselyä suoritettu ajassa:\t" + str(round(time.time() - alku_aika,6)) + " sek")
     pass
 
-#Pääohjelma
-print("-" * 80)
-print("Tervetuloa paketinseurantajärjestelmään!")
-while True: 
-    
+
+def listaakomennot():
+
     print("(1) Alusta tietokanta")
     print("(2) Lisää uusi paikka")
     print("(3) Lisää uusi asiakas")
@@ -179,9 +226,16 @@ while True:
     print("(8) Hae paikasta tapahtumien määrä tiettynä päivänä")
     print("(9) Suorita tietokannan tehokkuustesti")
     print("(x) Poistu ohjelmasta")
-    print("Syötä haluamasi komento : ")
-    komento = input()
+    pass
 
+#Pääohjelma
+print("-" * 80)
+print("Tervetuloa paketinseurantajärjestelmään!")
+listaakomennot()
+while True: 
+    print("Komento L näyttää koko komentolistauksen")
+    komento = input("Syötä haluamasi komento : ")
+    
     if (komento == '1'):      
         alusta(c)
     elif (komento == '2'):
@@ -199,8 +253,10 @@ while True:
     elif (komento == '8'):
         haeTapahtumienMäärä(c)
     elif (komento == '9'):
-        suoritaTehokkuustesti(c)
-    elif (komento == 'x'):
+        suoritaTehokkuustesti(c)        
+    elif (komento.lower() == 'l'):
+        listaakomennot()
+    elif (komento.lower() == 'x'):
         kanta.close()
         print("-" * 80)
         print("Kiitos käytöstä!")
